dab34f47488c2d897e366e6e2b1d98884d2aac58
prash26 <prashant@sammantrana.com>
2022-08-09 11:28:25 +0530

    update

--- server/controller/booksController.js
+++ server/controller/booksController.js
@@ -29,3 +29,23 @@ exports.deleteBook = async function(req,res){
     var resData = await Books.findByIdAndDelete(req.params.id);
     return res.status(200).json({resData});
 }
+
+exports.updateBook = async function(req, res) {
+    await check('name', "Book name is required!").notEmpty().run(req);
+    await check('author', 'Author name is required!').notEmpty().run(req);
+    await check('id', 'Book Id is required!').notEmpty().run(req);
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).send({ errors: errors.array(), message: 'Could not update book!' });
+    }
+
+    let book = await Books.findById(req.body.id);
+    if(book) {
+        book.name = req.body.name;
+        book.author = req.body.author;
+        await book.save();
+        return res.status(200).json({ "res" :"ok"});
+    } else {
+        return res.status(422).json({ "res" :"Book not found!"});
+    }
+}
--- server/routes/books.js
+++ server/routes/books.js
@@ -6,10 +6,12 @@ const checkPermission = require('../utils').checkPermission;
 module.exports = (router) => {
     router.route('/books')
         .post([logger, validateToken, checkPermission], booksController.books);
+    router.route('/books/update')
+        .post([logger, validateToken, checkPermission], booksController.updateBook);
     router.route('/bookById')
         .get([validateToken], booksController.findBook);
     router.route('/bookAll')
         .get([validateToken], booksController.findBooks);
     router.route('/books/delete/:id')
-        .delete([validateToken, checkPermission],booksController.deleteBook);
+        .delete([logger, validateToken, checkPermission],booksController.deleteBook);
 };
